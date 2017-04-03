6d1767d7c6aed10f36bf784f4d41d48f51ff40ca
Tom Leonardsson <tom.leo@hotmail.com>
2017-04-03 14:27:12 +0200

    basic functions for database

---
+++ ServerProgram/src/database/Database.java
@@ -0,0 +1,41 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Database {
+	private Connection connection;
+	
+	private ResultSet resultSet;
+	
+	private Statement statement;
+	
+	public Database(String database, String user, String password) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost/" + database + "?" + "user="+user+"&password="+password);
+	}
+	
+	public ResultSet execute(String s) throws SQLException {
+		statement = connection.createStatement();
+		return statement.executeQuery(s);
+	}
+	
+	public int getInt(String selection, String column) throws SQLException {
+		resultSet = execute(selection);
+		return resultSet.getInt(column);
+	}
+	
+	public String getString(String selection, String column) throws SQLException {
+		resultSet = execute(selection);
+		return resultSet.getString(column);
+	}
+	
+	public void close() throws SQLException {
+		connection.close();
+		resultSet.close();
+		statement.close();
+	}
+}
