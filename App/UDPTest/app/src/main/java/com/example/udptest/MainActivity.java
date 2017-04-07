33ffa22f57e061159eaec7ba56d83f977f7a0eab
Unknown <George--Albert@hotmail.com>
2017-04-07 11:27:19 +0200

    APP UDPTest
    Skickar strängen "1" till servern med IP-adress 10.2.19.28 med porten 4444.
    
    Vad som behövs testas:
    
    - att ta emot data-paket från servern och visa detta visuellt i appen på skärmen
    - att användaren själv anger IP och port
    - att fånga alla fel och åtgärda dessa så att appen inte kraschar

--- App/UDPTest/app/src/main/java/com/example/udptest/MainActivity.java
+++ App/UDPTest/app/src/main/java/com/example/udptest/MainActivity.java
@@ -20,8 +20,11 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * A login screen that offers login via email/password.
@@ -32,31 +35,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * Id to identity READ_CONTACTS permission request.
      */
     WifiManager w;
-    TextView status;
+
+
     InetAddress server_ip;
-    int server_port = 4444;
-    private String ipAddress = "192.168.0.12";
+    private int server_port = 4444;
+    private String ipAddress = "10.2.19.28";
     private byte[] message = new byte[1500];
     String statusText;
     String text;
 
     private EditText  mIPView;
+    private TextView status;
     private TextView  mStatusView;
 
 
     private AsyncTask<Void, Void, Void> async_udp;
     private boolean Server_Active = true;
-
-
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,15 +59,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         //populateAutoComplete();
 
-        status = (TextView) findViewById(R.id.ip);
-
+        status = (TextView) findViewById(R.id.status);
+        mStatusView =  (TextView) findViewById(R.id.message);
         w = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
 
         if (!w.isWifiEnabled()) {
             status.setText("switching ON wifi ");
             w.setWifiEnabled(true);
         } else {
-            status.setText("Its already ON ");
+            status.setText("WiFi is already ON ");
 
         }
 
@@ -89,7 +83,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 return false;
             }
         });
-        mStatusView =  (TextView) findViewById(R.id.message);
+
 
         findViewById(R.id.send_UDP_button).setOnClickListener(this); // calling onClick() method
         findViewById(R.id.button_begin_graph).setOnClickListener(this); // calling onClick() method
@@ -98,21 +92,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
     @Override
     public void onClick(View view) {
+        Log.d("IP-address", String.valueOf(ipAddress));
         //Send data
         switch (view.getId()) {
             case R.id.send_UDP_button:
+                Log.d("runUdpServer", "Running server");
+                runUdpServer();
+                break;
+            case R.id.button_begin_graph:
                 ipAddress = String.valueOf(mIPView.getText());
-                Log.d("IP-address", String.valueOf(ipAddress));
                 message[0] = 'H';
-                message[0] = 'e';
-                message[0] = 'l';
-                message[0] = 'l';
-                message[0] = 'o';
-                message[0] = '?';
+                message[1] = 'e';
+                message[2] = 'l';
+                message[3] = 'l';
+                message[4] = 'o';
+                message[5] = '?';
+                Log.d("DATAGRAM_PACKET",    "Constructs a DatagramPacket for receiving packets of length length.");
                 DatagramPacket p = new DatagramPacket(message, message.length);
                 DatagramSocket s = null;
                 try {
+                    Log.d("DATAGRAM_SOCKET", "Constructs a datagram socket and binds it to the specified port on the local host machine.");
                     s = new DatagramSocket(server_port);
+                    Log.d("RECEIVE", "Receives a datagram packet from this socket.");
                     s.receive(p);
                 } catch (SocketException e) {
                     e.printStackTrace();
@@ -125,24 +126,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Log.d("Udp tutorial", "message:" + text);
                 s.close();
                 break;
-            case R.id.button_begin_graph:
-                Log.d("runUdpServer", "Running server");
-                runUdpServer();
-                break;
             case R.id.button_refresh_status:
                 Log.d("BUTTON REFRESH", "button_refresh_status");
                 refreshStatus(mIPView);
                 break;
-        default:
-            Log.d("NO BUTTON", "and still onClick event ecevuted, something wrong...");
-            break;
+            default:
+                Log.d("NO BUTTON", "and still onClick event ecevuted, something wrong...");
+                break;
         }
     }
 
+    /**
+     * Get IP address from first non-localhost interface
+     * @param //ipv4  true=return ipv4, false=return ipv6
+     * @return  address or empty string
+     */
+    public static String getIPAddress(boolean useIPv4) {
+        try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for (InetAddress addr : addrs) {
+                    if (!addr.isLoopbackAddress()) {
+                        String sAddr = addr.getHostAddress();
+                        //boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
+                        boolean isIPv4 = sAddr.indexOf(':')<0;
+
+                        if (useIPv4) {
+                            if (isIPv4)
+                                return sAddr;
+                        } else {
+                            if (!isIPv4) {
+                                int delim = sAddr.indexOf('%'); // drop ip6 zone suffix
+                                return delim<0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception ex) { } // for now eat exceptions
+        return "";
+    }
+
+
 
     public void refreshStatus(View view)
-    {
-        status.setText(statusText);
+        {
+        mStatusView.setText(statusText);
     }
 
     public void runUdpServer()
@@ -150,46 +180,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         int x;
         WifiInfo info = w.getConnectionInfo();
-        status.setText(" ");
+        mStatusView.setText(" ");
         status.append("\n\nWiFi Status: " + info.toString());
 
         x = info.getIpAddress();
+        Log.d("IP ADDRESS", String.valueOf(x));
         String str1 = info.getMacAddress();
-
+        Log.d("MAC ADDRESS", str1);
         status.append("\n\nmac address===" + str1 + "  ,ip===" + x);
-
+        Log.d("AFTER STATUS APPEND", "debug");
         try {
+            Log.d("CC1", "debug");
             server_ip = InetAddress.getByName(ipAddress); // ip of THE OTHER DEVICE - NOT THE PHONE
+            Log.d("ipAddress", String.valueOf(ipAddress));
+            Log.d("server_ip", String.valueOf(server_ip));
 
         } catch (UnknownHostException e) {
+            Log.d("UnknownHostException", "debug");
             status.append("Error at fetching inetAddress");
         }
 
+
+
+
+
+
+        Log.d("B1", "debug");
         async_udp = new AsyncTask<Void, Void, Void>() {
             @Override
             protected Void doInBackground(Void... params) {
 
-                String str2 = "TEST MESSAGE !!!";
-                byte b1[];
-                b1 = new byte[100];
+                String str2 = "1";
+                byte b1[] = new byte[100];
                 b1 = str2.getBytes();
-                //DatagramPacket p1 = new DatagramPacket(b1, b1.length, server_ip, server_port);
+                DatagramPacket p1 = new DatagramPacket(b1, b1.length, server_ip, server_port);
 
-
+                Log.d("B2", "debug");
                 try {
-                    //DatagramSocket s = new DatagramSocket(server_port, server_ip);
-                    DatagramSocket s = new DatagramSocket(server_port);
+                    Log.d("A0", String.valueOf(server_port));
+                    Log.d("A0", String.valueOf(server_ip));
+                    DatagramSocket s = new DatagramSocket();
+                    //DatagramSocket s = new DatagramSocket(server_port);
+                    Log.d("A1", "debug1");
                     s.connect(server_ip, server_port);
 
-                    //DatagramPacket p0 = new DatagramPacket(b1, b1.length, InetAddress.getByName("192.168.43.xxx"), server_port);
-                    //s.send(p0);
+                    Log.d("A1", "debug2");
+                    DatagramPacket p0 = new DatagramPacket(b1, b1.length, InetAddress.getByName(ipAddress), server_port);
+
+                    Log.d("A1", "debug3");
+                    s.send(p0);
                     //The above two line can be used to send a packet - the other code is only to recieve
-
-                    DatagramPacket p1 = new DatagramPacket(b1,b1.length);
-                    s.receive(p1);
-
+                    Log.d("A2", "s.connect(server_ip, server_port)");
+                    //DatagramPacket p1 = new DatagramPacket(b1,b1.length);
+                    Log.d("A3", "DatagramPacket p1 = new DatagramPacket(b1,b1.length)");
+                    Log.d("wifi IP", getIPAddress(true));
+                    //s.receive(p1);
+                    Log.d("A4", "s.receive(p1)");
                     s.close();
+                    Log.d("A5", "s.close()");
                     b1=p1.getData();
+
                     String str = new String( b1);
 
                     server_port = p1.getPort();
@@ -227,7 +277,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         {
             async_udp.execute();
         }
-        status.setText(statusText); //need to set out here, as above is in an async thread
+        mStatusView.setText(statusText); //need to set out here, as above is in an async thread
 
     }
 }
