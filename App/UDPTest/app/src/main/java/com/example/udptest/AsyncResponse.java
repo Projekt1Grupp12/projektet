80fc519674cbaa5ecae620825072c8fef8a3f03d
Unknown <George--Albert@hotmail.com>
2017-04-20 09:04:42 +0200

    Code cleaning

--- App/UDPTest/app/src/main/java/com/example/udptest/AsyncResponse.java
+++ App/UDPTest/app/src/main/java/com/example/udptest/AsyncResponse.java
@@ -4,8 +4,10 @@ package com.example.udptest;
  * Created by George on 2017-04-19.
  */
 
+/**
+ * Interface used as a listner to AsyncTask so that MainActivity receives
+ * the resulting output from AsyncTask from the server.
+ */
 public interface AsyncResponse {
-
     void postResult(String asyncresult);
-
 }
--- App/UDPTest/app/src/main/java/com/example/udptest/MainActivity.java
+++ App/UDPTest/app/src/main/java/com/example/udptest/MainActivity.java
@@ -1,70 +1,63 @@
 package com.example.udptest;
 
+
 import android.content.Context;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
-import android.net.wifi.p2p.WifiP2pManager;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.TextUtils;
+
 import android.util.Log;
+
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
+
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.util.Collections;
-import java.util.List;
+
+import android.net.wifi.WifiManager;
 
 /**
  * A login screen that offers login via email/password.
  */
 //AppCompatActivity is a base class for activities that use the support library action bar features.
 //View.OnClickListener is an interface definition for a callback to be invoked when a view is clicked.
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, AsyncResponse{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
     //WIFIManager class provides the primary API for managing all aspects of Wi-Fi connectivity
     WifiManager w;
 
-    //InetAddress class represents an Internet Protocol (IP) address.
-    //IP is a lower-level protocol on which protocols like UDP and TCP are built.
-    InetAddress server_ip;
-    private int server_port = 4444;
-    private String ipAddress = "10.2.19.28";
-    private byte[] message = new byte[1500];
-    String statusText = null;
+    private String ipAddress = "10.2.19.28"; //Server IP-address
+    private int server_port = 4444; //Server portnumber
+
+    String message = null;
+
     private SendActivity send;
     private ReceiveActivity receive;
-    MobileInfo mInfo;
+
+    MobileInfo mInfo; //used to get the phones IP-address
 
     private boolean hasPlayerID = false;
     private static int playerID;
     private AsyncResponse asyncDelegate  = new AsyncResponse() {
         @Override
         public void postResult(String asyncresult) {
+            Log.i("ASYNC_RESULT", asyncresult);
             if(!hasPlayerID){
                 hasPlayerID = true;
-                playerID = Integer.parseInt(asyncresult);
+                try {
+                    playerID = Integer.parseInt(asyncresult);
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                    hasPlayerID = false;
+                    Log.i("NumberFormatException", e.getMessage());
+                }
             }
-
-            Log.i("ASYNC_RESULT", asyncresult);
         }
     };
-
 
     private EditText mIPView;
     private TextView status;
@@ -105,7 +98,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mInfo = new MobileInfo();
         Log.d("Phone IP", mInfo.getIPAddress(true));
         send = new SendActivity(ipAddress, server_port);
-        statusText = "First contact established...";
+        message = "First contact established...";
         send.execute("0");
         //execute the async task
         ReceiveActivity receive = new ReceiveActivity(ipAddress, server_port, asyncDelegate);
@@ -117,14 +110,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         Log.i("onCreate", "FINISHED");
     }
-
-    //this method has to be implement so that the results can be called to this class
-    public void postResult(String asyncresult){
-        //This method will get call as soon as your AsyncTask is complete. asyncresult will be your result.
-        Log.i("ASYNC_RESULT", asyncresult);
-    }
-
-
 
     private void turnOnWiFi(WifiManager w){
         if (!w.isWifiEnabled()) {
@@ -141,62 +126,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         send = new SendActivity(ipAddress, server_port);
+        String message = "";
         //Send data
         switch (view.getId()) {
-                //If send_UDP button is pressed, call send_UDP_button method.
+            //If send_UDP button is pressed, call send_UDP_button method.
             case R.id.send_UDP_button:
-                statusText = "0";
+                message = "0";
                 if(!isEmpty(mIPView)) {
                     ipAddress = String.valueOf(mIPView.getText());
                 }
-                send.execute(statusText);
-                //runUdpServer();
+                send.execute(message);
                 break;
             case R.id.btn_Green://1
-                statusText = "1";
-                //runUdpServer();
-                send.execute(statusText);
+                message = "1";
+                send.execute(message);
                 break;
             case R.id.btn_Yellow://2
-                statusText = "2";
-                //runUdpServer();
-                send.execute(statusText);
+                message = "2";
+                send.execute(message);
                 break;
             case R.id.btn_Red://3
-                statusText = "3";
-                //runUdpServer();
-                send.execute(statusText);
+                message = "3";
+                send.execute(message);
                 break;
             default:
                 Log.d("NO CASES MATCH", "onCklick event executed, something is wrong...");
                 break;
         }
-
-
-
         //Wait for response from server
         receive = new ReceiveActivity(ipAddress, server_port, asyncDelegate);
         receive.execute();
+
         try {
             Thread.sleep(5);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.gc();
+       System.gc(); //Garbage collector
     }
 
-    private boolean isEmpty(EditText etText) {
-        if (etText.getText().toString().trim().length() > 0)
+    private boolean isEmpty(EditText setText) {
+        if (setText.getText().toString().trim().length() > 0)
             return false;
         return true;
     }
-
-    //Update status of the view
-    public void refreshStatus(View view)
-        {
-        mStatusView.setText(statusText);
-    }
-
 }
 
 
