63a655a4c6da43431246f2177a1b450e8c25f2e2
Unknown <George--Albert@hotmail.com>
2017-05-08 14:45:53 +0200

    Bug ID_MF5 almost solved
    Createing new Thread already in MainMenu Activity.
    
    All games will use this Thread and this thread will also listen to if the Serve connection is lost.
    
    The problem that remains is when the User is redirected to MainFrame then the method onResume() is called. The Applications should start fresh and forget all information.
    
    Also this has to be tested if it works when playing the game. It should.

--- App/ColorGame/app/src/main/java/com/example/anna/colorgame/MainFrame.java
+++ App/ColorGame/app/src/main/java/com/example/anna/colorgame/MainFrame.java
@@ -120,9 +120,27 @@ public class MainFrame extends AppCompatActivity {
         player.setChoosenIP(editIPText.getText().toString());
         player.setName(editNameText.getText().toString());
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop()");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy()");
+    }
+
     /*
-    This method sends data(IP, Name, userID) to next activity using Intent class.
-     */
+        This method sends data(IP, Name, userID) to next activity using Intent class.
+         */
     private void startNextActivity() {
         Log.d(TAG, "Creating new intent and sending data");
         Intent intent = new Intent(this, MainMenu.class);
