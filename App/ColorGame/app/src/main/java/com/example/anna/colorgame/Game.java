a607e868bc859af798ebb6033f06a63f8ae9de53
Unknown <George--Albert@hotmail.com>
2017-05-09 11:58:47 +0200

    .java: Auto stash before merge of "Game.java" and "DebugApplication"

---
+++ App/ColorGame/app/src/main/java/com/example/anna/colorgame/Game.java
@@ -0,0 +1,114 @@
+package com.example.anna.colorgame;
+
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+/**
+ * Created by George on 2017-05-09.
+ */
+
+public class Game extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = "debugGame";
+    private static final String RED = "3";
+    private static final String YELLOW = "2";
+    private static final String GREEN = "1";
+    private static final String SEMICOLON = ";";
+    private String data = "";
+    private Player player;
+    private MediaPlayer mpGood;
+    private MediaPlayer mpBad;
+
+    private AsyncResponse delegate = new AsyncResponse() {
+        /*
+        Method that has result from AsyncTask as parameter.
+        It is used to update TextView in this activity.
+         */
+        @Override
+        public void postResult(String result) {
+            TextView textViewMove = (TextView) findViewById(R.id.textViewMove);
+            Log.d(TAG, "Inside updateUI call 2");
+            textViewMove.setText(result);
+            Log.d(TAG, "Text updated");
+            //Music is played here.
+            if(mpGood.isPlaying() || mpBad.isPlaying()) {
+                mpGood.stop();
+                mpBad.stop();
+            }
+            try {
+                AssetFileDescriptor afd;
+                if (result.contains("GOOD")) {
+                    afd = getAssets().openFd("goodmove.mp3");
+                    mpGood.reset();
+                    mpGood.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
+                    mpGood.prepare();
+                    mpGood.start();
+                } else if (result.contains("BAD")) {
+                    afd = getAssets().openFd("badmove.mp3");
+                    mpBad.reset();
+                    mpBad.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+                    mpBad.prepare();
+                    mpBad.start();
+                }
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+
+    public Game(){
+        //Initiate audioclips
+        mpGood = MediaPlayer.create(this, R.raw.goodmove);
+        mpBad = MediaPlayer.create(this, R.raw.badmove);
+    }
+
+    /*
+    This method is called when one of the buttons is clicked in the GUI.
+    Which button is pushed is stored in data variable.
+    Then the semicolon and userID is added to data string and sent to startAsyncTask method.
+     */
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.greenButton:
+                data = GREEN;
+                break;
+            case R.id.yellowButton:
+                data = YELLOW;
+                break;
+            case R.id.redButton:
+                data = RED;
+                break;
+        }
+        String temp = data;
+        data += SEMICOLON + player.getUserID();
+        startAsyncTask(temp, data);
+    }
+
+    /*
+    This method is used to create new instance of a ConnectToServer class and send data as
+    parameter to doInBackground method of that class.
+     */
+    public void startAsyncTask(String color, String data){
+        Log.d(TAG, "Create task. " + color);
+        ConnectToServer runner = new ConnectToServer(player.getChoosenIP(), delegate);
+        System.gc();
+        Log.d(TAG, "Execute task. " + color);
+        runner.execute(data);
+    }
+
+    public AsyncResponse getDelgate(){
+        return this.delegate;
+    }
+}
