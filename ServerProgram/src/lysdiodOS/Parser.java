c8dad36214eb0d12a6abc9e81b57328a3d184147
Tom Leonardsson <tom.leo@hotmail.com>
2017-05-30 13:09:30 +0200

    parser

--- ServerProgram/src/lysdiodOS/Parser.java
+++ ServerProgram/src/lysdiodOS/Parser.java
@@ -26,6 +26,8 @@ public class Parser {
 			new Instruction("CBI", 0b111110, new int[]{REGISTER, NUMBER}),
 	};
 	
+	private final String ENTER_INSTRUCTION = "-2";
+	
 	private final String PINS_PORTS[] = new String[]{
 		"PORTD",
 		"DDRD",
@@ -40,11 +42,71 @@ public class Parser {
 	
 	private String total;
 	private String currentLine;
+	private String input;
+	
+	private String currentPart;
+	
+	private int count;
+	
+	private boolean firstBitHalf = false;
+	
+	private Instruction currentInstruction;
+	
+	public Parser() {
+		currentPart = "000";
+		total = "";
+		currentLine = "";
+	}
+	
+	public void update(String input) {
+		if(!input.equals("-2"))
+			for(int i = 0; i < 3; i++) {
+				if(Integer.parseInt(input.charAt(0) + "") == (i+1)) {
+					if(currentPart.charAt(i) == '0') {
+						String t = "";
+						for(int j = 0; j < 3; j++) {
+							t += (j == i) ? '1' : currentPart.charAt(j);
+						}
+						currentPart = t;
+					} else {
+						String t = "";
+						for(int j = 0; j < 3; j++) {
+							t += (j == i) ? '0' : currentPart.charAt(j);
+						}
+						currentPart = t;
+					}
+				}
+			}
+		
+		if(input.equals(ENTER_INSTRUCTION)) {
+			currentLine += currentPart;
+			System.out.println(currentLine);
+			if(!firstBitHalf) {
+				if(count == 0) {
+					currentInstruction = getInstruction(Integer.parseInt(currentLine));
+				}
+				
+				currentLine += " ";
+				
+				count += 1;
+				
+				if(currentInstruction != null && count > currentInstruction.getParameters().length) {
+					total += currentLine + "\n";
+					currentLine = "";
+					currentPart = "000";
+					count = 0;
+				}
+			}
+			
+			firstBitHalf = !firstBitHalf;
+			input = "";
+		}
+	}
 	
 	public String translateParamter(int value, int index, int parameter) {
 		if(getInstruction(value).getParameters()[index] == REGISTER) {
 			if(parameter <= 32) return "r"+parameter;
-			if(parameter <= 41) return PINS_PORTS[parameter-36];
+			if(parameter <= 41) return PINS_PORTS[parameter-33];
 			else return parameter + "";
 		} else if(getInstruction(value).getParameters()[index] == STRING) {
 			return (char)(64+parameter) + "";
@@ -75,5 +137,14 @@ public class Parser {
 		}
 		
 		return null;
+	}
+	
+	public static void main(String[] args) {
+		Parser p = new Parser();
+		
+		p.update("2");
+		p.update("-2");
+		p.update("1");
+		p.update("-2");
 	}
 }
